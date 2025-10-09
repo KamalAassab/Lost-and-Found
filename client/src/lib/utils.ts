71120c74ea8ca42c9feb78a38426565c3dbcc2a5
KamalAassab <kamalaassab2002@gmail.com>
2025-10-09 04:03:55 +0100

    🔄 REVERT: Undo createSlug function changes

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -23,8 +23,7 @@ export function createSlug(text: string) {
     .normalize('NFD')
     .replace(/[\u0300-\u036f]/g, '')
     .replace(/[^a-z0-9]+/g, '-')
-    .replace(/(^-|-$)+/g, '')
-    .replace(/t-shirts?/g, 'tshirts'); // Convert t-shirts to tshirts
+    .replace(/(^-|-$)+/g, '');
 }
 
 export function applyHoodiePromotion(items: any[]) {
